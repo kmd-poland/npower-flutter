51ae5ecd4d872def5ec4593e2c996b7e67d10010
Michal Mazur (MMU) <MMU@kmd.dk>
2019-03-05 11:46:35 +0100

    Added avatar to visit

--- lib/data/visit.dart
+++ lib/data/visit.dart
@@ -1,6 +1,7 @@
 import 'dart:core';
 
 class Visit {
+  final String avatar;
   final String firstName;
   final String lastName;
   final DateTime startTime;
@@ -8,6 +9,7 @@ class Visit {
   final List<double> coordinates;
 
   const Visit({
+    this.avatar,
     this.firstName,
     this.lastName,
     this.startTime,
@@ -17,6 +19,7 @@ class Visit {
 
   factory Visit.fromJson(Map<String, dynamic> json) {
     return Visit(
+      avatar: json['avatar'],
       firstName: json['firstName'],
       lastName: json['lastName'],
       startTime: DateTime.parse(json['startTime']),
